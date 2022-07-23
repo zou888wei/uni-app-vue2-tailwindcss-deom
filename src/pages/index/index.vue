93aa83561fe44d3e3050f612993973613efc84cc
邹伟 <zouwei@git.com.cn>
2022-07-24 00:54:40 +0800

    teat: 初始化，无ts版本

---
+++ src/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		<view>{{title}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
